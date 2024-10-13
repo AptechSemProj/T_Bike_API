f5207772651347526db17788a09f181bb72bd5f6
VuTuanAnh <tuananhcho1234@gmail.com>
2024-10-13 10:32:58 +0700

    Fix

--- src/main/java/com/sem/proj/tbike/api/core/brand/dto/BrandCreation.java
+++ src/main/java/com/sem/proj/tbike/api/core/brand/dto/BrandCreation.java
@@ -1,8 +1,8 @@
 package com.sem.proj.tbike.api.core.brand.dto;
 
-import com.sem.proj.tbike.api.util.RequestObject;
+import com.sem.proj.tbike.api.util.RequestType;
 
-@RequestObject
+@RequestType
 public class BrandCreation {
 
 }
