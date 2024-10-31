fc19372a396e0851762aba26ce9103977df9b479
VuTuanAnh <tuananhcho1234@gmail.com>
2024-10-31 11:25:26 +0700

    fix ProductApiUrls

---
+++ src/main/java/se/pj/tbike/core/api/product/conf/ProductApiUrls.java
@@ -0,0 +1,7 @@
+package se.pj.tbike.core.api.product.conf;
+
+import se.pj.tbike.core.config.Urls;
+
+public class ProductApiUrls {
+	public static final String PRODUCT_API = Urls.API_PREFIX + "/products";
+}
